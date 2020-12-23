7837621fb98a13619c1ceb19882eb2197e94a766
lazzypanda <lazzypanda@yandex.ru>
2020-12-23 22:15:08 +0300

    feat(3): Added task PageComponent  try-2

--- 02-components/02-MeetupPageComponent/MeetupAgenda.js
+++ 02-components/02-MeetupPageComponent/MeetupAgenda.js
@@ -1,14 +1,30 @@
-// import { MeetupAgendaItem } from './MeetupAgendaItem.js';
+import { MeetupAgendaItem } from './MeetupAgendaItem.js';
 
 export const MeetupAgenda = {
   name: 'MeetupAgenda',
 
   template: `
     <div class="meetup-agenda">
-      <div class="meetup-agenda__item"></div>
+    <p
+      v-if="agenda.length === 0"
+      class="meetup-agenda__empty"
+    >Программа пока пуста, но когда-нибудь в ней обязательно что-нибудь появится!</p
+    >
+      <meetup-agenda-item
+        v-for="agendaEvent in agenda"
+        :key="agendaEvent.id"
+        :agendaEvent="agendaEvent"
+      ></meetup-agenda-item>
     </div>`,
 
-  // components
+  components: {
+    MeetupAgendaItem,
+  },
 
-  // props
+  props: {
+    agenda: {
+      type: Array,
+      default: [],
+    },
+  },
 };
