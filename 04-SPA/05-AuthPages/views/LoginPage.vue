c03d3f74a156d5e3c5446a87c745e94b70d121f9
lazzypanda <lazzypanda@yandex.ru>
2021-01-06 23:01:08 +0300

    feat(05): Add AuthPages task

--- 04-SPA/05-AuthPages/views/LoginPage.vue
+++ 04-SPA/05-AuthPages/views/LoginPage.vue
@@ -1,13 +1,71 @@
 <template>
-  <form class="form"></form>
+  <form @submit.prevent="onSubmit" class="form">
+    <div class="form-group">
+      <label class="form-label">Email</label>
+      <div class="input-group">
+        <input
+          v-model="email"
+          class="form-control"
+          type="email"
+          placeholder="demo@email"
+        />
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="form-label">Пароль</label>
+      <div class="input-group">
+        <input
+          v-model="password"
+          class="form-control"
+          type="password"
+          placeholder="password"
+        />
+      </div>
+    </div>
+    <div class="form__buttons">
+      <button type="submit" class="button button_primary button_block">
+        Войти
+      </button>
+    </div>
+    <div class="form__append">
+      Нет аккаунта?&nbsp;<router-link :to="{ name: 'register' }" class="link"
+        >Зарегистрируйтесь</router-link
+      >
+    </div>
+  </form>
 </template>
 
 <script>
-// import { login } from '../data';
+// https://github.com/js-tasks-ru/vue-course-materials/blob/master/meetups-markup/login.html
+import { login } from '../data';
 
 export default {
   name: 'LoginPage',
+
+  data() {
+    return {
+      email: '',
+      password: '',
+    };
+  },
+
+  methods: {
+    async onSubmit() {
+      if (!this.email) {
+        alert('Требуется ввести Email');
+      } else if (!this.password) {
+        alert('Требуется ввести пароль');
+      } else {
+        let resToJSON = await login(this.email, this.password);
+
+        if (!resToJSON.error) {
+          alert(resToJSON.fullname); //при успешном запросе выводится полное имя пользователя;
+        } else {
+          alert(resToJSON.message); //при неуспешном запросе выводится сообщение «Неверные учетные данные»
+        }
+      }
+    },
+  },
 };
 </script>
-
 <style scoped></style>
--- 04-SPA/05-AuthPages/views/RegisterPage.vue
+++ 04-SPA/05-AuthPages/views/RegisterPage.vue
@@ -1,12 +1,91 @@
 <template>
-  <form class="form"></form>
+  <form @submit.prevent="onSubmit" class="form">
+    <div class="form-group">
+      <label class="form-label">Email</label>
+      <div class="input-group">
+        <input v-model="email" class="form-control" type="email" />
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="form-label">Имя</label>
+      <div class="input-group">
+        <input v-model="fullname" class="form-control" type="text" />
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="form-label">Пароль</label>
+      <div class="input-group">
+        <input v-model="password" class="form-control" type="password" />
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="form-label">Повтор пароля</label>
+      <div class="input-group">
+        <input v-model="passwordConfirm" class="form-control" type="password" />
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="checkbox"
+        ><input v-model="Accept_EULA" type="checkbox" />Я согласен с условиями
+        <span></span
+      ></label>
+    </div>
+    <div class="form__buttons">
+      <button type="submit" class="button button_primary">
+        Зарегистрироваться
+      </button>
+    </div>
+    <div class="form__append">
+      Уже есть аккаунт?
+      <router-link :to="{ name: 'login' }" class="link">Войдите</router-link>
+    </div>
+  </form>
 </template>
 
 <script>
-// import { register } from '../data';
+import { register } from '../data';
+// https://github.com/js-tasks-ru/vue-course-materials/blob/master/meetups-markup/register.html
 
 export default {
   name: 'RegisterPage',
+
+  data() {
+    return {
+      email: '',
+      fullname: '',
+      password: '',
+      passwordConfirm: '',
+      Accept_EULA: false,
+    };
+  },
+
+  methods: {
+    async onSubmit() {
+      if (!this.email) {
+        alert('Требуется ввести Email');
+      } else if (!this.fullname) {
+        alert('Требуется ввести полное имя');
+      } else if (!this.password) {
+        alert('Требуется ввести пароль');
+      } else if (this.password != this.passwordConfirm) {
+        alert('Пароли не совпадают');
+      } else if (this.Accept_EULA == false) {
+        alert('Требуется согласиться с условиями');
+      } else {
+        let resToJSON = await register(
+          this.email,
+          this.fullname,
+          this.password,
+        );
+
+        if (!resToJSON.error) {
+          alert(resToJSON.id); // при успешном запросе выводится ID зарегистрированного пользователя
+        } else {
+          alert(resToJSON.message); // при неуспешном запросе выводится сообщение из ошибки
+        }
+      }
+    },
+  },
 };
 </script>
 
