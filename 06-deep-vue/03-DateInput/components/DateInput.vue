1c10bbbcdf330ba817af5e548b9d216816de091e
javascriptru <67475495+jsru-1@users.noreply.github.com>
2021-01-23 17:45:15 +0300

    Added solution for task: DateInput

--- 06-deep-vue/03-DateInput/components/DateInput.vue
+++ 06-deep-vue/03-DateInput/components/DateInput.vue
@@ -1,6 +1,11 @@
 <template>
-  <AppInput>
-    <!-- Так можно передать все слоты в дочерний компонент -->
+  <AppInput
+    v-bind="$attrs"
+    v-on="$listeners"
+    :value="inputValue"
+    :type="type"
+    @input.native="handleUpdate"
+  >
     <template v-for="slot of Object.keys($slots)" v-slot:[slot]>
       <slot :name="slot" />
     </template>
@@ -16,8 +21,73 @@ export default {
   components: { AppInput },
 
   props: {
-    // Чтобы value не было в списке $attrs, его можно указать в списке параметров
+    type: {
+      type: String,
+      default: 'date',
+      validator: (type) => ['date', 'datetime-local', 'time'].includes(type),
+    },
     value: {},
+    valueAsDate: {
+      type: Date,
+    },
+    valueAsNumber: {
+      type: Number,
+    },
+  },
+
+  computed: {
+    listeners() {
+      return {
+        ...this.$listeners,
+        input: undefined,
+        change: undefined,
+      };
+    },
+
+    date() {
+      return (
+        (this.valueAsNumber ? new Date(this.valueAsNumber) : null) ||
+        this.valueAsDate
+      );
+    },
+
+    dateValue() {
+      return this.date.toISOString().substring(0, 10);
+    },
+
+    timeValue() {
+      return this.date.toISOString().substring(11, 16);
+    },
+
+    timeWithSecondsValue() {
+      return this.date.toISOString().substring(11, 19);
+    },
+
+    inputValue() {
+      if (!this.date) {
+        return this.value;
+      }
+      if (this.type === 'date') {
+        return this.dateValue;
+      } else if (this.type === 'time') {
+        return (this.$attrs['step'] && this.$attrs['step'] % 60 !== 0) ? this.timeWithSecondsValue : this.timeValue;
+      } else if (this.type === 'datetime-local') {
+        return `${this.dateValue}T${this.timeValue}`;
+      }
+      return null;
+    },
+  },
+
+  methods: {
+    handleUpdate($event) {
+      this.$emit('update:valueAsNumber', $event.target.valueAsNumber);
+      this.$emit(
+        'update:valueAsDate',
+        this.type !== 'datetime-local'
+          ? $event.target.valueAsDate
+          : new Date($event.target.valueAsNumber),
+      );
+    },
   },
 };
 </script>
